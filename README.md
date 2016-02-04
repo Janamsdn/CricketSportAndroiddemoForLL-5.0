c6ef200fbe37f6a7972e14adf68d7c8142a0d755
Janamsdn <janamsdn@gmail.com>
2016-02-04 20:31:28 +0000

    Lollipop 5.0 features-SportsDemo
    the latest Lollipop 5.0 features such as Toolbar, Recycler View,
    Material Design, Navigation Drawer, Floating Action Button and more

---
+++ README.md
@@ -0,0 +1,1 @@
+the latest Lollipop 5.0 features such as Toolbar, Recycler View, Material Design, Navigation Drawer, Floating Action Button and more
---
+++ app/src/main/java/com/jay/iplcricket/VideoEntry.java
@@ -0,0 +1,15 @@
+package  com.jay.iplcricket;
+
+/**
+ *
+ */
+public class VideoEntry {
+
+    public final String text;
+    public final String videoId;
+
+    public VideoEntry(String text, String videoId) {
+        this.text = text;
+        this.videoId = videoId;
+    }
+}
